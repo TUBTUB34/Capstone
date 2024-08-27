08932be2fbbeacfe487d81c25e52c808b2bfa0b8
TUBTUB34 <alex.j.weir34@gmail.com>
2024-08-27 11:26:29 -0700

    Started Brain PCB

--- Tests/Arduion_Slave_Test/Arduion_Slave_Test.ino
+++ Tests/Arduion_Slave_Test/Arduion_Slave_Test.ino
@@ -1,17 +1,18 @@
 #include <Wire.h>
-#include <Adafruit_NeoPixel.h>
 
-#define SLAVE_ADDRESS 0x09  // Unique I2C address for this Arduino
+#define SLAVE_ADDRESS 0x0a  // Unique I2C address for this Arduino
 
-Adafruit_NeoPixel pixels(1, PIN_NEOPIXEL);
 
-char currentColor = 'O';  // Store the current color
+int voltage = 12;  //test voltage
+int current = 2;   //test current
+int power = voltage*current;
+char command;
 
 void setup() {
+  Serial.begin(9600);
   Wire.begin(SLAVE_ADDRESS);  // Initialize the I2C bus with the given address
   Wire.onRequest(requestEvent);  // Register function to handle requests
   Wire.onReceive(receiveEvent);  // Register function to handle received data
-  pixels.begin();  // Initialize the pixel
   
 }
 
@@ -19,25 +20,30 @@ void loop() {
   delay(100);  // Main loop is empty; rely on interrupts
 }
 
-// Function to handle requests from the master (Raspberry Pi)
+
 void requestEvent() {
-  // Send the current color back to the master
-  Wire.write(currentColor);
+  // Send  back to the master
+  if (command == 'V'){
+    Wire.write(voltage);
+  }
+  else if (command == 'C') {
+    Wire.write(current);
+  }4
+  else if (command == 'P') {
+    Wire.write(power);
+  }
+  
 }
 
 // Function to handle incoming data from the master (Raspberry Pi)
 void receiveEvent(int howMany) {
-  if (howMany >= 2) {  // Expecting at least two bytes
-    char firstByte = Wire.read();  // Read the first byte
-    char secondByte = Wire.read();  // Read the second byte
-    Serial.println(secondByte);
-    if (secondByte != '\0') {
-      currentColor = secondByte;
-      setColor(currentColor);
-    }
-  }
+    char garbage = Wire.read();  // Read the first byte
+    command = Wire.read();  // Read the second byte
+    Serial.println(command);
+
 }
 
+/*
 void setColor(char color) {
   if (color == 'R') {
     pixels.setPixelColor(0, pixels.Color(255, 0, 0));  // Red
@@ -50,3 +56,4 @@ void setColor(char color) {
   }
   pixels.show();
 }
+*/
